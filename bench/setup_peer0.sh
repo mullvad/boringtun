93ce80130f41e73106a31d6d05cc001e69cf05e9
David Lönnhager <david.l@mullvad.net>
2024-11-29 10:25:15 +0100

    test bench

---
+++ bench/setup_peer0.sh
@@ -0,0 +1,22 @@
+PEER0_IP=10.100.123.1
+
+sudo ip l del dev peer0
+
+WG_SUDO=1 cargo run --bin boringtun-cli peer0
+
+# Dump conf
+
+cat <<-EOF >peer0.conf.temp
+[Interface]
+PrivateKey = GOxrlZilSgaR5yF9Xqu94JyuTom4G00Mgg6S2UmYb04=
+#pubkey cmDzGsB71iWCi05QdgcF+HVmjfc3+u1ER3nDH7erpCc=
+
+[Peer]
+PublicKey = 3wYfE1TbG5v0td0QtTTpGw52rT3C7sOe/MsZ7ZH2CBU=
+#privkey aOwPSa4ISy87rgpaJsS8cUnLX4gsi8TGVPZCax4+gFo=
+Endpoint = 192.168.1.97:51820
+AllowedIPs = 10.100.123.2/32
+EOF
+
+sudo ip a add dev peer0 $PEER0_IP
+sudo wg setconf peer0 ./peer0.conf.temp
---
+++ bench/setup_peer1.sh
@@ -0,0 +1,20 @@
+PEER1_IP=10.100.123.2
+
+sudo ip l del dev peer1
+
+WG_SUDO=1 cargo run --bin boringtun-cli peer1
+
+# Dump conf
+
+cat <<-EOF >peer1.conf.temp
+[Interface]
+PrivateKey = aOwPSa4ISy87rgpaJsS8cUnLX4gsi8TGVPZCax4+gFo=
+ListenPort = 51820
+
+[Peer]
+PublicKey = cmDzGsB71iWCi05QdgcF+HVmjfc3+u1ER3nDH7erpCc=
+AllowedIPs = 10.100.123.1/32
+EOF
+
+sudo ip a add dev peer1 $PEER1_IP
+sudo wg setconf peer1 ./peer1.conf.temp
